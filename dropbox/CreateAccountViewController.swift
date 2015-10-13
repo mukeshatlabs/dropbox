a79db6454ae95d8a98517289eec6186eac9492ee
Mukesh Jain <mukeshj@gmail.com>
2015-10-12 18:58:20 -0700

    initial version

---
+++ dropbox/CreateAccountViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateAccountViewController.swift
+//  dropbox
+//
+//  Created by Mukesh Jain on 10/10/15.
+//  Copyright © 2015 walmart. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBAction func backToWelcome(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ dropbox/SignInRootViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SignInRootViewController.swift
+//  dropbox
+//
+//  Created by Mukesh Jain on 10/10/15.
+//  Copyright © 2015 walmart. All rights reserved.
+//
+
+import UIKit
+
+class SignInRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSegueWithIdentifier("signIn", sender: self)
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
